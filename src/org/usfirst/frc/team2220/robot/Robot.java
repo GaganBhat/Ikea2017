146038975ce7b893205059338888350991b027c4
SoProTheyGoWoah <gaganbhat6@gmail.com>
2017-11-17 17:45:36 -0600

    Better Drive

--- src/org/usfirst/frc/team2220/robot/Robot.java
+++ src/org/usfirst/frc/team2220/robot/Robot.java
@@ -1,18 +1,23 @@
 
 package org.usfirst.frc.team2220.robot;
+import java.util.*;
 
 import org.usfirst.frc.team2220.robot.subsystems.DefaultDrive;
 import org.usfirst.frc.team2220.robot.subsystems.TwilightDrive;
 
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.TalonControlMode;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.cscore.CvSink;
 import edu.wpi.cscore.CvSource;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Utility;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -32,35 +37,57 @@ public class Robot extends IterativeRobot {
 
 	public static OI oi;
 	private AHRS NavX;
-	
+	private static CANTalon rDriveMaster;
+	private static CANTalon rDriveSlave;
+	private static CANTalon lDriveMaster;
+	private static CANTalon lDriveSlave;
+	long millisToRun = 1000;
 	double [] defaultVal = new double[0];
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
+
 	
 	//NetworkTable Vals
 	NetworkTable visionTable;
-	double[] widthA = visionTable.getNumberArray("width", defaultVal);
-	double width = widthA[0];
+	//double[] widthA = visionTable.getNumberArray("width", defaultVal);
+	//double width = widthA[0];
 
 	@Override
 	public void robotInit() {
 		//USB Camera Initialization and Setup
-		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture();
+		UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(0);
 		camera.setResolution(240, 180);
-		camera.setFPS(30);
+		camera.setFPS(10);
 		CvSink cvsink = CameraServer.getInstance().getVideo();
+		camera.setBrightness(0);
+		camera.setExposureManual(1);
 		
-		//NavX Initialization and Setupp
+		//NavX Initialization and Setup
 		NavX = new AHRS(SPI.Port.kMXP);
 		double yawVal = NavX.getYaw();
 		double angleVal = NavX.getAngle();
 		SmartDashboard.putNumber("Yaw", yawVal);
-		
 		//Initializing the values of the Vision Contours report
 		visionTable = NetworkTable.getTable("GRIP/myContoursReport");
 		
 		oi = new OI();
 		SmartDashboard.putData("Auto mode", chooser);
+		
+		rDriveMaster = new CANTalon(RobotMap.rightMaster);
+		rDriveSlave = new CANTalon(RobotMap.rightSlave);
+		lDriveMaster = new CANTalon(RobotMap.leftMaster);
+		lDriveSlave = new CANTalon(RobotMap.leftSlave);
+		
+		lDriveSlave.changeControlMode(TalonControlMode.Follower);
+		lDriveSlave.set(lDriveMaster.getDeviceID());
+		
+		rDriveSlave.changeControlMode(TalonControlMode.Follower);
+		rDriveSlave.set(rDriveMaster.getDeviceID());
+		
+		rDriveMaster.setInverted(false);
+		lDriveMaster.setInverted(true);
+		//RobotDrive maindrive = new RobotDrive(lDriveMaster, rDriveMaster);
+		
 		
 	}
 
@@ -75,7 +102,7 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
-		SmartDashboard.putNumber("Width", width);
+		//SmartDashboard.putNumber("Width", width);
 	}
 
 
@@ -85,11 +112,14 @@ public class Robot extends IterativeRobot {
 		
 		if (autonomousCommand != null)
 			autonomousCommand.start();
+		
 	}
 
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
+		
+		
 	}
 
 	@Override
@@ -99,7 +129,8 @@ public class Robot extends IterativeRobot {
 			autonomousCommand.cancel();
 		
 		Scheduler.getInstance().removeAll();
-		DefaultDrive.init();
+	
+		
 		
 	}
 
@@ -107,12 +138,14 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void teleopPeriodic() {
 		//TwilightDrive.getInstance().controlTwilightDrive();
-		DefaultDrive.Drive();
-		
+		//DefaultDrive.Drive();
+		lDriveMaster.set(oi.getDriverJoystick().getRawAxis(1));
+		rDriveMaster.set(oi.getDriverJoystick().getRawAxis(5));
 		SmartDashboard.putNumber("LeftAxis0", oi.getDriverJoystick().getRawAxis(1));
 		SmartDashboard.putNumber("RightAxis5", oi.getDriverJoystick().getRawAxis(5));
-		SmartDashboard.putNumber("Width", width);
+		//SmartDashboard.putNumber("Width", width);
 		
+		//maindrive.tankDrive(Robot.oi.getDriverJoystick().getRawAxis(1),Robot.oi.getDriverJoystick().getRawAxis(5));
 		Scheduler.getInstance().run();
 		
 	}
